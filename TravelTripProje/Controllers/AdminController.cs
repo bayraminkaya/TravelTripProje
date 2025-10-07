f28e3405448f03dcdde1969d0aab749c547c5271
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2025-10-07 23:47:48 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var b1 = c.Blogs.Find(id);
+            return View("BlogGetir", b1);
+        }
+
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Aciklama=b.Aciklama;
+            blg.Baslik=b.Baslik;
+            blg.Tarih=b.Tarih;
+            blg.BlogImage=b.BlogImage;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var b1 = c.Yorumlars.Find(id);
+            return View("YorumGetir", b1);
+        }
+
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yorum = c.Yorumlars.Find(y.ID);
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail;
+            yorum.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
+        public ActionResult Index()
+        {
+            by.Deger1=c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            //var bloglar = c.Blogs.ToList();
+            return View(by);
+        }
+        
+        public ActionResult BlogDetay(int id)
+        {
+             
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1=c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2=c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult YorumYap(Yorumlar y)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Hatalı durumlarda aynı detay sayfasına geri dön
+                TempData["YorumHata"] = "Lütfen tüm alanları doldurun.";
+                return RedirectToAction("BlogDetay", new { id = y.Blogid });
+            }
+
+            c.Yorumlars.Add(y);
+            c.SaveChanges();
+
+            TempData["YorumOk"] = "Yorumunuz kaydedildi.";
+            // ÖNEMLİ: Redirect! Böylece F5’te tekrar POST yapılmaz.
+            return RedirectToAction("BlogDetay", new { id = y.Blogid });
+        }
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+
+    }
+}
